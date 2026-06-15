344f1ea129d1e9f38d5e28bf37693ade7fe9ba7b
cristiancondoriapaza <56841357+cristiancondoriapaza@users.noreply.github.com>
2026-06-15 06:40:33 -0400

    subir archivos

---
+++ WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(528, 428);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(554, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(528, 428);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 513);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CARGAR IMAGEN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 512);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TEXTURA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(660, 513);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "AÑADIR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(958, 513);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 50);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "MOSTRAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 500);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 526);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(144, 552);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(554, 513);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(831, 513);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 607);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = "server=server;user=user;pwd=pwd;database=imagenes;";
+
+        private int ventana = 20;
+        private int promR, promG, promB;
+        private int cr, cg, cb; 
+        private int xyR, xyG, xyB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand("SELECT descripcion FROM textura", con))
+            {
+                con.Open();
+                using (SqlDataReader lector = cmd.ExecuteReader())
+                {
+                    while (lector.Read())
+                    {
+                        comboBox1.Items.Add(lector["descripcion"].ToString());
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            using (Bitmap bmp = new Bitmap(pictureBox1.Image))
+            {
+                promR = 0; promG = 0; promB = 0;
+                int x = e.X;
+                int y = e.Y;
+                int mitadVentana = ventana / 2;
+
+                int startX = Math.Max(0, x - mitadVentana);
+                int endX = Math.Min(bmp.Width, x + mitadVentana);
+                int startY = Math.Max(0, y - mitadVentana);
+                int endY = Math.Min(bmp.Height, y + mitadVentana);
+
+                int conteoPixeles = 0;
+                for (int i = startX; i < endX; i++)
+                {
+                    for (int j = startY; j < endY; j++)
+                    {
+                        Color cp = bmp.GetPixel(i, j);
+                        promR += cp.R;
+                        promG += cp.G;
+                        promB += cp.B;
+                        conteoPixeles++;
+                    }
+                }
+                if (conteoPixeles > 0)
+                {
+                    promR /= conteoPixeles;
+                    promG /= conteoPixeles;
+                    promB /= conteoPixeles;
+
+                    double sumSqR = 0, sumSqG = 0, sumSqB = 0;
+
+                    for (int i = startX; i < endX; i++)
+                    {
+                        for (int j = startY; j < endY; j++)
+                        {
+                            Color cp = bmp.GetPixel(i, j);
+                            sumSqR += Math.Pow(cp.R - promR, 2);
+                            sumSqG += Math.Pow(cp.G - promG, 2);
+                            sumSqB += Math.Pow(cp.B - promB, 2);
+                        }
+                    }
+
+                    cr = (int)Math.Sqrt(sumSqR / conteoPixeles);
+                    cg = (int)Math.Sqrt(sumSqG / conteoPixeles);
+                    cb = (int)Math.Sqrt(sumSqB / conteoPixeles);
+                }
+
+                Color pixelCentral = bmp.GetPixel(x, y);
+                xyR = pixelCentral.R;
+                xyG = pixelCentral.G;
+                xyB = pixelCentral.B;
+
+                textBox1.Text = "R: " + promR.ToString() + " | Tex: " + cr.ToString();
+                textBox2.Text = "G: " + promG.ToString() + " | Tex: " + cg.ToString();
+                textBox3.Text = "B: " + promB.ToString() + " | Tex: " + cb.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            ProcesarYMostrarImagen(promR, promG, promB, cr, cg, cb);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox4.Text)) return;
+
+            comboBox1.Items.Add(textBox4.Text);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sql = "INSERT INTO textura (r, g, b, descripcion, cr, cg, cb) VALUES (@r, @g, @b, @desc, @cr, @cg, @cb)";
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    cmd.Parameters.AddWithValue("@r", promR);
+                    cmd.Parameters.AddWithValue("@g", promG);
+                    cmd.Parameters.AddWithValue("@b", promB);
+                    cmd.Parameters.AddWithValue("@desc", textBox4.Text);
+                    cmd.Parameters.AddWithValue("@cr", cr);
+                    cmd.Parameters.AddWithValue("@cg", cg);
+                    cmd.Parameters.AddWithValue("@cb", cb);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || pictureBox1.Image == null) return;
+
+            int R = 0, G = 0, B = 0;
+            int CR = 0, CG = 0, CB = 0;
+            string descripcion = comboBox1.SelectedItem.ToString();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT r, g, b, cr, cg, cb FROM textura WHERE descripcion = @desc";
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    cmd.Parameters.AddWithValue("@desc", descripcion);
+                    con.Open();
+                    using (SqlDataReader lector = cmd.ExecuteReader())
+                    {
+                        if (lector.Read())
+                        {
+                            R = Convert.ToInt32(lector["r"]);
+                            G = Convert.ToInt32(lector["g"]);
+                            B = Convert.ToInt32(lector["b"]);
+
+                            if (lector["cr"] != DBNull.Value) CR = Convert.ToInt32(lector["cr"]);
+                            if (lector["cg"] != DBNull.Value) CG = Convert.ToInt32(lector["cg"]);
+                            if (lector["cb"] != DBNull.Value) CB = Convert.ToInt32(lector["cb"]);
+                        }
+                    }
+                }
+            }
+
+            ProcesarYMostrarImagen(R, G, B, CR, CG, CB);
+        }
+
+        private void ProcesarYMostrarImagen(int targetR, int targetG, int targetB, int targetCR, int targetCG, int targetCB)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp1 = new Bitmap(bmp.Width, bmp.Height);
+
+            int pR, pG, pB;
+            int pCR, pCG, pCB;
+            int area = ventana * ventana;
+
+            for (int i = 0; i <= bmp.Width - ventana; i += ventana)
+            {
+                for (int j = 0; j <= bmp.Height - ventana; j += ventana)
+                {
+                    pR = 0; pG = 0; pB = 0;
+
+                    for (int k = i; k < i + ventana; k++)
+                    {
+                        for (int l = j; l < j + ventana; l++)
+                        {
+                            Color cp = bmp.GetPixel(k, l);
+                            pR += cp.R;
+                            pG += cp.G;
+                            pB += cp.B;
+                        }
+                    }
+                    pR /= area;
+                    pG /= area;
+                    pB /= area;
+
+                    double sumSqR = 0, sumSqG = 0, sumSqB = 0;
+                    for (int k = i; k < i + ventana; k++)
+                    {
+                        for (int l = j; l < j + ventana; l++)
+                        {
+                            Color cp = bmp.GetPixel(k, l);
+                            sumSqR += Math.Pow(cp.R - pR, 2);
+                            sumSqG += Math.Pow(cp.G - pG, 2);
+                            sumSqB += Math.Pow(cp.B - pB, 2);
+                        }
+                    }
+                    pCR = (int)Math.Sqrt(sumSqR / area);
+                    pCG = (int)Math.Sqrt(sumSqG / area);
+                    pCB = (int)Math.Sqrt(sumSqB / area);
+
+                    int tolColor = 50;   // Tolerancia equilibrada para color
+                    int tolTextura = 15; // Tolerancia equilibrada para rugosidad
+
+                    bool coincideColor = Math.Abs(targetR - pR) < tolColor &&
+                                         Math.Abs(targetG - pG) < tolColor &&
+                                         Math.Abs(targetB - pB) < tolColor;
+
+                    bool coincideTextura = Math.Abs(targetCR - pCR) < tolTextura &&
+                                           Math.Abs(targetCG - pCG) < tolTextura &&
+                                           Math.Abs(targetCB - pCB) < tolTextura;
+
+                    for (int k = i; k < i + ventana; k++)
+                    {
+                        for (int l = j; l < j + ventana; l++)
+                        {
+                            if (coincideColor && coincideTextura)
+                            {
+                                bmp1.SetPixel(k, l, Color.Black);
+                            }
+                            else
+                            {
+                                bmp1.SetPixel(k, l, bmp.GetPixel(k, l));
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (pictureBox2.Image != null) pictureBox2.Image.Dispose();
+            pictureBox2.Image = bmp1;
+            bmp.Dispose();
+        }
+    }
+}
